02ba2a5576fdd56637d6a3fe599494aabdda90f6
Fernando070514 <fs484282@gmail.com>
2026-04-07 19:58:30 -0300

    Add frmCategorias form with event handlers

---
+++ frmCategorias.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NexStock.UI
+{
+    public partial class frmCategorias : Form
+    {
+        public frmCategorias()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCriar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
