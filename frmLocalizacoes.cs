5b656193b3486369d0eaf66419000ea9c2af8a71
Fernando070514 <fs484282@gmail.com>
2026-04-07 20:01:04 -0300

    Add frmLocalizacoes form with event handlers

---
+++ frmLocalizacoes.cs
@@ -0,0 +1,17 @@
+namespace NexStock.UI
+{
+    public partial class frmLocalizacoes : Form
+    {
+        private void txtBuscarLocalizaco_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+     
+    }
+}
